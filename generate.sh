725191cca33f0a076d7d6d00d43d6ce03bde98ab
Luiz Carvalho <lucarval@redhat.com>
2025-06-11 16:22:53 -0400

    Add script to generate template files
    This ensure changes to the base template are propagated to each
    template.
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- generate.sh
+++ generate.sh
@@ -5,5 +5,8 @@ ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # pull in latest pipeline run definition
 $ROOTDIR/scripts/update-tekton-definition
 
+# update generated files in templates
+$ROOTDIR/scripts/update-templates
+
 # get gitops templates
 echo SKIP $ROOTDIR/scripts/import-gitops-template
